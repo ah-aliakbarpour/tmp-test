d2dedb74f8cd6c063d1a925a0199a74002d4ea15
Amirhossein Aliakbarpour <ah.aliakbarpour@gmail.com>
2024-10-02 18:47:20 +0330

    Migration create comments table

---
+++ database/migrations/2024_10_02_140018_create_comments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('post_id')
+                ->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->string('title');
+            $table->longText('body');
+            $table->boolean('approved')->default(false);
+            $table->timestamp('approved_at')->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
