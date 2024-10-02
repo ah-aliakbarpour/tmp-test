a5e7cb364ec9bf838b9cf58048b9bd1cd9b349d6
Amirhossein Aliakbarpour <ah.aliakbarpour@gmail.com>
2024-10-02 18:47:09 +0330

    Migration create posts table

---
+++ database/migrations/2024_10_02_131615_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('category_id')->nullable()
+                ->constrained()->cascadeOnUpdate()->nullOnDelete();
+            $table->string('title');
+            $table->longText('body');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
